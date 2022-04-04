d50d0b845a0ebbd9104c1c0b8caa4eec0a152739
CristopherGuzman <guzmncristopher@gmail.com>
2022-04-04 15:56:10 -0500

    ADD: form validation

--- js/modal.js
+++ js/modal.js
@@ -7,7 +7,11 @@ let btn = document.getElementById("shopButton");
 // Obtengo el elemento <span> que cierra el modal
 let span = document.getElementsByClassName("close")[0];
 
-let buttonBuy = document.getElementById("buttonBuy");
+// Obtengo el form
+const form = document.querySelector('form');
+
+// Obtengo el input de name = nombre
+let inputName = document.querySelector('input[name="nombre"]')
 
 // Cuando el usuario clickea el boton abrel el modal
 btn.onclick = function() {
@@ -30,13 +34,31 @@ window.onclick = function(event) {
 }
 
 //Cuando el usuario haga clic en el boton de Finalizar compra, cierra el modal y muestra alerta
-buttonBuy.onclick = function(){
-  modal.style.display = "none"
+const sendAlert = (name) => {
+  modal.style.display = "none";
   Swal.fire({
     title: 'Completado!',
-    text: 'Gracias por tu compra, te enviaremos un correo',
+    text: `Gracias por tu compra ${name} te enviaremos un correo`,
     icon: 'success',
     confirmButtonText: 'Cool'
   })
-  
 }
+
+// Cuando el usuario escribe dentro del input nombre se guarda en una variable
+let nombre;
+inputName.addEventListener('input', (e) => {
+  nombre = e.target.value.toUpperCase();
+})
+
+// Funciones que se ejecutan en el submit
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  sendAlert(nombre); 
+  localStorage.clear()
+  cart = [];
+  localStorage.setItem("cart", JSON.stringify(cart))
+  showCart();
+  updateCount();
+  updateTotalCart();
+  form.reset();
+})
